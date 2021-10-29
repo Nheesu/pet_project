60f7afb17949df4ef2be76fe02db595762197922
Nheesu <83944922+Nheesu@users.noreply.github.com>
2021-10-29 10:46:00 +0900

    Update searchWork.py

--- searchWork.py
+++ searchWork.py
@@ -10,10 +10,10 @@ def findCategory(num):
     headers = {
         'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
     }
-    random_num = random.sample(range(1, 411), num)
+    random_num = random.sample(range(1, 411), num)  // 1~411페이지 내에서 입력받은 num값만큼 추출한다.
     for i in random_num:
-        pageNo = str(i)
-        url = request_url + '?serviceKey=' + serviceKey + '&pageNo=' + pageNo
+        pageNo = "pageNo" +str(i)                  // 위에서 뽑아낸 값을 페이지로 입력
+        url = request_url + serviceKey + '&' + pageNo
 
         data = requests.get(url, headers=headers)
         soup = BeautifulSoup(data.text, 'html.parser')
@@ -23,9 +23,9 @@ def findCategory(num):
             for item in items:
                 category = item.select_one('subjectcategory')
                 category_name = category.text
-                if category_name not in categories:
-                    categories.append(category_name)
+                if category_name not in categories:     // categories에 해당 요소가 없으면
+                    categories.append(category_name)    // categories에 category_name을 추가
 
     return categories
 
-findCategory(10)
+findCategory(36)
