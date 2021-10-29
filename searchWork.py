521c30eb85f88fa9f4c6155cdf2c5ffd5ec05702
Nheesu <83944922+Nheesu@users.noreply.github.com>
2021-10-29 11:11:25 +0900

    Update searchWork.py

--- searchWork.py
+++ searchWork.py
@@ -28,4 +28,43 @@ def findCategory(num):
 
     return categories
 
+def searchKeyword(state, category):
+  search_object = list()
+  i=0
+  request_url ="http://api.kcisa.kr/openapi/service/rest/convergence2019/getConver03?"
+  serviceKey='서비스키 값'
+  keyword = 'keyword='+category
+  where = 'where='+state
+  
+  while True :
+    i+=1
+    pageNo = "pageNo="+ str(i)
+    url = request_url+ serviceKey + '&'+pageNo+'&'+ keyword+'&'+where
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
+    }
+    data = requests.get(url, headers= headers)
+    soup = BeautifulSoup(data.text, 'html.parser')
+    items = soup.select('items > item')
+
+    if len(items)==0 :
+      break 
+
+    for item in items :
+
+      if item.select_one('state').text == '정상':
+        title = item.select_one('title')
+        category = item.select_one('subjectcategory')
+        reference = item.select_one('reference')
+        affiliation = item.select_one('affiliation')
+        venue = item.select_one('venue')
+        new_info = {'title': title.text,
+                    'category' : category.text,
+                    'reference' : reference.text,
+                    'affiliation' : affiliation.text,
+                    'venue' : venue.text}
+        search_object.append(new_info)
+
+  return search_object
+
 findCategory(36)
